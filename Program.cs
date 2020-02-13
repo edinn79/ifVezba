28e0939421abc8b924231ff8fb31c3d47d844e6e
Edin Gilic <gilicedin@gmail.com>
2020-02-13 15:21:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifVezba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite vase puno ime");
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Unesite vase godine");
+            int godine = int.Parse(Console.ReadLine());
+
+            if (godine < 18)
+            {
+                Console.WriteLine(ime + " " + "je mladji od 18 godina, maloletan je i nije ozenjen");
+
+            }
+            else if (godine == 18)
+            {
+                Console.WriteLine(ime + " " + "ima tacno 18 godina, punoletan je i nije ozenjen");
+
+            }
+            else if (godine > 18 && godine < 90)
+            {
+                Console.WriteLine(ime + " " + "ima vise od 18 godina, punoletan je i ozenjen je ");
+            }
+
+            else
+            {
+                Console.WriteLine(ime + " " + "ima preko 90 godina i skoro je da umre");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
